16ec8707719ef71b47177b03d62487b13b97bf2c
Camilo <95003040+CamiloVelasquezBotero@users.noreply.github.com>
2026-04-08 02:43:00 +0000

    Adding /src and Model Movimiento

---
+++ src/Movimiento.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Movimiento {
+    public string Categoria;
+    public string Descripcion;
+    public string Monto;
+
+    public Movimiento(string categoria, string descripcion, decimal monto) {
+        Categoria = categoria;
+        Descripcion = descripcion;
+        Monto = Monto;
+    }
+}
